92555767301a1cc6a5937fdbde64dbc8dc3a1fa3
Daniel Milde <daniel@milde.cz>
2008-10-28 23:51:42 +0100

    new table classes

--- db/activeRecords/User.php
+++ db/activeRecords/User.php
@@ -17,26 +17,6 @@
 */
 class ActiveRecord_User extends Core_ActiveRecord
 {
-	/**
-	* $fields
-	*
-	* Array of fields in table.
-	* @var array $fields
-	*/
-	public $fields = array(
-		'id_user'   => array('visibility' => 0, 'type' => 'int',       'db' => 'int(11) unsigned'),
-		'id_group'  => array('visibility' => 1, 'type' => '->group',   'db' => 'int(11) unsigned'),
-		'name'      => array('visibility' => 1, 'type' => 'text',      'db' => 'varchar(100) NOT NULL'),
-		'password'  => array('visibility' => 1, 'type' => 'password',  'db' => 'varchar(100) NOT NULL'),
-		'fullname'  => array('visibility' => 1, 'type' => 'text',      'db' => 'varchar(100) NOT NULL'),
-		'phone'     => array('visibility' => 1, 'type' => 'text',      'db' => 'varchar(50) NULL'),
-		'email'     => array('visibility' => 1, 'type' => 'text',      'db' => 'varchar(100) NULL'),
-		'created'   => array('visibility' => 1, 'type' => 'datetime',  'db' => 'datetime NULL'),
-		'active'    => array('visibility' => 1, 'type' => 'checkbox',  'db' => 'int(11) unsigned default "0"'),
-	);
-
-	protected $table = 'user';
-
 	public function __construct($id_user=false)
 	{
 		parent::__construct('user',$id_user);
