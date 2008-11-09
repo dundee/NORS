c2ecc02702e8ed921aedeb90049119e0f23724af
Daniel Milde <daniel@milde.cz>
2008-11-09 23:58:21 +0100

    separation of Request into Request, Response and Router

---
+++ Core/Response.php
@@ -0,0 +1,155 @@
+<?php
+
+/**
+ * Core_Response
+ *
+ * @author Daniel Milde <daniel@milde.cz>
+ * @copyright Daniel Milde <daniel@milde.cz>
+ * @license http://www.opensource.org/licenses/gpl-license.php
+ * @package Core
+ */
+
+/**
+ * Core_Response
+ *
+ * @author Daniel Milde <daniel@milde.cz>
+ * @package Core
+ */
+class Core_Response
+{
+	/**
+	 * $instance
+	 * 
+	 * @var Core_Request $instance
+	 */
+	static private $instance;
+
+	/**
+	 * $session
+	 * 
+	 * @var Core_Session $session
+	 */
+	private $session;
+
+	/**
+	 * factory
+	 *
+	 * @return Core_Response
+	 */
+	static public function factory($class = FALSE)
+	{
+		if (isset(self::$instance)) {
+			return self::$instance;
+		}
+				
+		$class = 'Core_Response';
+		self::$instance = new $class;
+		return self::$instance;
+	}
+
+	/**
+	 * setGet
+	 * 
+	 * @param string $key Wrapper for writing $_...[$key]
+	 * @param mixed $value
+	 * @return void
+	 */
+	public function setGet($key, $value)
+	{
+		$this->setTo($key, $value, $_GET);
+	}
+
+	/**
+	 * setPost
+	 *  
+	 * @param string $key Wrapper for writing $_...[$key]
+	 * @param mixed $value
+	 * @return void
+	 */
+	public function setPost($key, $value)
+	{
+		$this->setTo($key, $value, $_POST);
+	}
+
+	/**
+	 * setSession
+	 * 
+	 * @param string $key Wrapper for writing $_...[$key]
+	 * @param mixed $value
+	 * @return void
+	 */
+	public function setSession($key, $value)
+	{
+		if (!$this->session) $this->session = Core_Session::singleton();
+		$this->setTo($key, $value, $_SESSION);
+	}
+
+	/**
+	 * setCookie
+	 * 
+	 * @param string $key Wrapper for writing $_...[$key]
+	 * @param mixed $value
+	 * @param int $time
+	 * @param string $directory
+	 * @return void
+	 */
+	public function setCookie($key, $value, $time = FALSE, $directory = '/')
+	{
+		if ($time === FALSE) $time = time() + Core_Config::singleton()->cookie->expiration;
+		setcookie($key,$value,$time,$directory);
+	}
+
+	/**
+	 * setGetFromArray
+	 * 
+	 * Loads $_GET from array.
+	 * 
+	 * @param mixed[] $array
+	 */
+	public function setGetFromArray($array){
+		$_GET = $array;
+	}
+	
+	/**
+	 * sendHeaders
+	 * 
+	 * Sends HTTP headers
+	 *
+	 * @param string $content_type
+	 * @param int $expires_in Number of seconds page should be cached
+	 * @return void
+	 */
+	public function sendHeaders($content_type = 'text/html', $expires_in = 0)
+	{
+		if (headers_sent()) return;
+		
+		if ($expires_in == 0) {
+			$expires = 0;
+			$modified = time();
+		}
+		else {
+			$expires = time() + $expires_in;
+			$modified = time() - $expires_in;
+		}
+		
+		header('Expires: ' . date("r", $expires));
+		header("Cache-Control: no-cache, must-revalidate, max-age=" . $expires_in);
+		header ("Last-Modified: " . date("r", $modified));
+		if ($expires_in == 0) header ("Pragma: no-cache");
+		header("Content-type: " . $content_type . "; charset=" . Core_Config::singleton()->encoding);
+		header("X-Powered-By: Core Framework");
+	}
+	
+	/**
+	 * setTo
+	 * 
+	 * @param string $key Wrapper for writing $_...[$key]
+	 * @param mixed $value
+	 * @param mixed $destination GET | POST | COOKIE | SESSION
+	 * @return void
+	 */
+	protected function setTo($key, $value, &$destination)
+	{
+		$destination[$key] = $value;
+	}
+}
---
+++ Core/Router/ModRewrite.php
@@ -0,0 +1,169 @@
+<?php
+
+/**
+ * Core_Router_ModRewrite
+ *
+ * @author Daniel Milde <daniel@milde.cz>
+ * @copyright Daniel Milde <daniel@milde.cz>
+ * @license http://www.opensource.org/licenses/gpl-license.php
+ * @package Core
+ */
+
+/**
+ * Core_Router_ModRewrite
+ *
+ * @author Daniel Milde <daniel@milde.cz>
+ * @package Core
+ */
+class Core_Router_ModRewrite extends Core_Router
+{
+	protected $routes;
+
+	protected $currentRoute;
+
+	/**
+	 * Constructor
+	 */
+	protected function __construct(){
+	}	
+
+	public function forward($params, $event = FALSE){
+		return $this->genUrl(FALSE, $event, FALSE, $params, TRUE);
+	}
+
+	public function redirect($module, $event, $route = FALSE, $params = FALSE, $inherit_params = FALSE, $moved = FALSE){
+		if($moved) header("HTTP/1.1 301 Moved Permanently");
+		header("Location: ".$this->genUrl($module, $event, $route, $params, $inherit_params, TRUE));
+		header("Connection: close");
+		exit(0);
+	}
+
+	public function genURL($module = FALSE,
+	                       $event = FALSE,
+	                       $routeName = FALSE,
+	                       $other_args = FALSE,
+	                       $inherit_params = FALSE,
+	                       $in_header = FALSE)
+	{
+		if (is_array($routeName)) throw new Exception('Wrong usage');
+
+		//init
+		$routeName = $routeName ? $routeName : $this->currentRoute;
+		$delimiter = $in_header ? '&' : '&amp;';
+		$_GET = is_array($_GET) ? $_GET : array();
+		$other_args = is_array($other_args) ? $other_args : array();
+		$module = $module ? $module : $_GET['module'];
+		$event  = $event  ? $event  : $_GET['event'];
+
+		//prepare args
+		$args = array_merge( $other_args, array('module' => $module,
+		                                        'event'  => $event) );
+		if ($inherit_params) $args = array_merge($_GET,$args);
+
+		$route = $this->routes[$routeName];
+		$urlForm = $route['url']; //URL form of route...e.g.: ':module/:event'
+
+		//create cool URL according to URL form
+		$url_parts = explode('/', $urlForm);
+		$url = APP_URL;
+		$url .= substr(APP_URL, strlen(APP_URL)-1, 1) == '/' ? '' : '/'; //slash on end
+
+		foreach ($url_parts as $part) {
+			if (substr($part, 0, 1) == ':') { //variable part
+				$part = substr($part, 1); //remove colon
+				$part_value = $args[$part];
+			} else { //static part
+				$part_value = $part;
+			}
+
+			if ($part_value == '__default') continue;
+			if (!$part_value && //no value for this part
+			    isset($route['defaults']) &&
+			    isset($route['defaults']->{$part})
+			    ) $part_value = $route['defaults']->{$part}; //default values
+
+			if ($part_value) $url .= $part_value . '/';
+			//echo $part . ' - ' . $args[$part] .' - '. $url.'<br />';
+			unset($args[$part]);
+		}
+
+		//after ?
+		$url2 = '';
+		foreach($args as $key=>$value){
+			if (
+			$key == 'browser' ||
+			$key == 'module' ||
+			$value == '__default'
+			) continue;
+			$url2 .= ($url2 ? $delimiter : '')   . $key;
+			$url2 .= ($value==='' ? '' : '=') . stripslashes($value);
+		}
+		$url .= $url2 ? '?'.$url2 : '';
+		return $url;
+	}
+
+	public function decodeUrl(Core_Request $request){
+		//prepare routes
+		$config = Core_Config::singleton();
+		foreach($config->routes as $name=>$route){
+			$this->routes[$name] = array('url'=>$route->format,'defaults'=>$route->defaults);
+		}
+
+		//decode URL
+		$directory = eregi_replace('http://([^/]+)','',APP_URL); //directory where is app placed
+		$url = $request->getServer('REQUEST_URI');
+
+		if (strlen($directory) > 1) { //app in subdir
+			$url = str_replace($directory.'/','',$url);
+		} else { //in root
+			$url = substr($url,1);
+		}
+
+		$arr = explode('?',$url);
+		$event = $arr[0];
+		$params = explode('/',$event);
+
+		if (!iterable($this->routes)) throw new UnderflowException("No routes defined");
+
+		//find matching route
+		foreach($this->routes as $name=>$route){
+			$urlForm = $route['url'];
+			$urlForm = eregi_replace(':([^/]*)','([^/]*)',$urlForm);
+			if ( eregi('^'.$urlForm.'/?$', $event) ) { //route matches
+				$this->currentRoute = $name;
+				break;
+			}
+		}
+
+		if (!$this->currentRoute) $this->currentRoute = 'default';
+		$route = $this->routes[$this->currentRoute];
+		if (!iterable($route)) throw new UnderflowException("Route ".$this->currentRoute." empty");
+
+		$parts = explode('/',$route['url']);
+		$i = 0;
+		foreach ($parts as $key) {
+			if (substr($key,0,1) != ':') {
+				$i++;
+				continue; //static part of URL
+			}
+			$key = substr($key,1);
+			$_GET[$key] = isset($params[$i]) ? $params[$i] : FALSE;
+			$i++;
+		}
+
+		foreach ($route['defaults'] as $k => $v) {
+			if (isset($_GET[$k]) && $_GET[$k]) continue;
+			$_GET[$k] = $v;
+		}
+
+		$_GET['event'] = isset($_GET['event']) ? $_GET['event'] : '__default';
+
+		//canonical URL? Redirect if not
+		$url = $request->getUrl();
+		$url = str_replace('&','&amp;',$url);
+		if ($this->genUrl($_GET['module'], $_GET['event'], FALSE, $_GET) != $url){
+			//echo $this->genUrl($_GET['module'], $_GET['event'], FALSE, $_GET).' - '.$url;
+			$this->redirect($_GET['module'], $_GET['event'], FALSE,$_GET,FALSE,TRUE);
+		}
+	}
+}
