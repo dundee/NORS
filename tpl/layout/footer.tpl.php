ffa357fa9a6e11a32bb10058d5138bf9977b72e6
Daniel Milde <daniel@milde.cz>
2008-12-26 19:58:45 +0100

    frontend - footer

--- tpl/layout/footer.tpl.php
+++ tpl/layout/footer.tpl.php
@@ -2,13 +2,9 @@
 
 echo '<div id="footer" class="cleared">
 	<div id="footer-main">
-		<p>Powered by <a href="http://core-php.com/">Core framework '.coreVersion().'</a> &copy;2007-'.date("Y").' <a href="http://milde.cz">Daniel Milde</a> aka Dundee</p>
+		<p>Powered by <a href="http://norsphp.com/">NORS '.norsVersion().'</a> &copy;2007-'.date("Y").' <a href="http://milde.cz">Daniel Milde</a> aka Dundee</p>
 
 	</div><!-- footer-main -->
-	<div id="footer-side">
-		<p><a href="">Podmínky použití</a> | <a href="">Nápověda</a> | <a href=""><strong>O projektu</strong></a> | <a href="">Napište nám</a></p>
-
-	</div><!-- footer-side -->
 </div><!-- footer -->
 
 
--- tpl/layout/footer_admin.tpl.php
+++ tpl/layout/footer_admin.tpl.php
@@ -5,7 +5,7 @@ echo '</div>
 echo '<div id="feet"><!-- feet -->'."\n".'
   <div id="feet_in"></div>'."\n";
 Core_Debug::showInfo();
-echo '<br />Powered by <a href="http://core-php.com/">Core framework '.coreVersion().'</a> &copy; 2005-'.date("Y").' Daniel Milde aka Dundee'."\n".'
+echo '<br />Powered by <a href="http://core-php.com/">NORS '.norsVersion().'</a> &copy; 2007-'.date("Y").' Daniel Milde aka Dundee'."\n".'
 </div><!-- end feet -->'."\n";
 
 //echo '</div><!-- end inner -->'."\n";
