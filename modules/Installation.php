5c16c9fe4388051cb46aaaeb43a6f45c704431d6
Daniel Milde <daniel@milde.cz>
2009-01-10 21:08:59 +0100

    installation fix - host

--- modules/Installation.php
+++ modules/Installation.php
@@ -19,17 +19,17 @@ class Installation extends Core_Module
 {
 	public $headerTplFile = 'header_admin.tpl.php';
 	public $footerTplFile = 'footer_admin.tpl.php';
-	
+
 	public $css = array(
 		'normal' => array('admin.css', 'forms.css'),
 		'ie6'    => array(),
 		'ie7'    => array(),
 		'print'  => array(),
 	);
-	
+
 	public $js = array('jquery.js',
 	                   'help.js');
-	
+
 	public $helpers = array('Form', 'Menu');
 
 	public $cache = 0;
@@ -48,40 +48,40 @@ class Installation extends Core_Module
 	{
 		$this->tplFile = 'admin_installation.tpl.php';
 		if ($this->config->db->user) throw new RuntimeException('Allready installed', 401);
-		
+
 		$host = $user = $pass = $db = $prefix = $user_name = $user_password = FALSE;
 		$host = 'localhost';
 		$prefix = 'nors4_';
-		
+
 		$errors = '';
 		if ($this->request->getPost('send')) {
 			do {
 				$this->config->debug->error_reporting = E_ERROR;
-				
-				$host          = $this->request->getPost('host');
+
+				$dbhost        = $this->request->getPost('host');
 				$user          = $this->request->getPost('user');
 				$pass          = $this->request->getPost('password');
 				$db            = $this->request->getPost('db');
 				$prefix        = $this->request->getPost('prefix');
 				$user_name     = $this->request->getPost('user_name');
 				$user_password = $this->request->getPost('user_password');
-				
+
 				mysql_connect($host, $user, $pass);
 				if (mysql_errno() == 1045) {
 					$errors .= __('wrong_db_user') . ': ' . mysql_error();
 					break;
 				}
-				
+
 				mysql_select_db($db);
 				if (mysql_errno() == 1049) {
 					$errors .= __('wrong_db_name') . ': ' . mysql_error();
 					break;
 				}
-				
+
 				//everythink ok, saving
-				
+
 				$this->config->debug->error_reporting = E_ALL;
-				
+
 				//save new configuration
 				include(APP_PATH . '/cache/config.yml.php.cache.php');
 				$config = $data;
@@ -89,10 +89,10 @@ class Installation extends Core_Module
 
 				$config[$host]['db']['user']         = $user;
 				$config[$host]['db']['password']     = $pass;
-				$config[$host]['db']['host']         = $host;
+				$config[$host]['db']['host']         = $dbhost;
 				$config[$host]['db']['database']     = $db;
 				$config[$host]['db']['table_prefix'] = $prefix;
-				
+
 				Core_Parser_YML::write($config, APP_PATH . '/config/config.yml.php');
 				//chmod(APP_PATH . '/config/config.yml.php', 0777);
 				$this->config->read(APP_PATH . '/config/config.yml.php', TRUE);
@@ -104,16 +104,16 @@ class Installation extends Core_Module
 					$user = new $class();
 					$user->create();
 				}
-				
+
 				$user = new ActiveRecord_User();
 				$user->name = $user_name;
 				$user->password = $user_password;
 				$user->active = 1;
 				$user->save();
-				
+
 				$this->router->redirect('post', '__default', 'default');
 			} while (FALSE);
-			
+
 			$this->setData('dbhost', $host);
 			$this->setData('dbuser', $user);
 			$this->setData('dbpassword', $pass);
@@ -122,11 +122,11 @@ class Installation extends Core_Module
 			$this->setData('user_name', $user_name);
 			$this->setData('user_password', $user_password);
 		}
-		
+
 		if ($errors) $this->response->setPost('send', FALSE);
-		
+
 		$this->setData('errors', $errors);
-		
+
 		$this->setData('dbhost', $host);
 		$this->setData('dbuser', $user);
 		$this->setData('dbpassword', $pass);
