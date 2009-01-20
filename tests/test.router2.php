659a6841b2fd9b4ffb2a7a67e003fa8f6d62e292
Daniel Milde <daniel@milde.cz>
2009-01-20 15:59:47 +0100

    router unit test for absolute URL

---
+++ tests/test.router2.php
@@ -0,0 +1,33 @@
+<?php
+$_SERVER = array();
+$_SERVER['PHP_SELF'] = '/nors4/index.php';
+$_SERVER['REQUEST_URI'] = 'http://localhost/nors4/post/'; //!!!!!!!!!!!!! also possible
+$_SERVER['SCRIPT_NAME'] = '/nors4/index.php';
+$_SERVER['HTTP_HOST'] = 'localhost';
+
+define('HIGH_PERFORMANCE', 0);
+error_reporting(E_ERROR | E_PARSE);
+ini_set('display_errors', 1);
+
+define('APP_PATH', dirname(__FILE__) . '/..');
+require_once('../Core/Functions.php');
+
+setUrlPath();
+
+Core_Config::singleton()->read(APP_PATH . '/config/config.yml.php');
+
+
+$router   = Core_Router::factory();
+$request  = Core_Request ::factory();
+
+$router->decodeUrl($request, FALSE);
+
+/* ************* TEST ************** */
+
+echo $router->genUrl('administration', 'edit', 'default', array('id'=>'5')) . ENDL . "<br />";
+echo $router->genUrl('administration', 'add', FALSE, array('x'=>4)) . ENDL . "<br />";
+echo $router->forward(array('id'=>10)) . ENDL . "<br />";
+echo $router->forward(array('id'=>FALSE)) . ENDL . "<br />";
+
+echo ENDL;
+?>
