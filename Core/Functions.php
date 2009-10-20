a9cedea517049480e07e536035d4ef662bc6d7e9
Daniel Milde <daniel@milde.cz>
2009-10-20 21:36:02 +0200

    request JSON support

--- Core/Functions.php
+++ Core/Functions.php
@@ -222,72 +222,7 @@ function echor($string)
 }
 
 if (!function_exists('json_encode')) {
-
-	/**
-	 * Encode string to JSON
-	 */
-	function json_encode_string($in_str)
-	{
-		$in_str = addslashes($in_str);
-		mb_internal_encoding("UTF-8");
-		$convmap = array(0x80, 0xFFFF, 0, 0xFFFF);
-		$str = "";
-		for ($i = mb_strlen($in_str)-1; $i >= 0; $i--) {
-			$mb_char = mb_substr($in_str, $i, 1);
-			if (mb_ereg("&#(\\d+);", mb_encode_numericentity($mb_char, $convmap, "UTF-8"), $match)) {
-				$str = sprintf("\\u%04x", $match[1]) . $str;
-			} else {
-				$str = $mb_char . $str;
-			}
-		}
-		$str = str_replace("\r\n", '\r\n', $str);
-		$str = str_replace("\t", '\t', $str);
-		return $str;
-	}
-
-	/**
-	 * Encode Array to JSON
-	 */
-	function json_encode($arr)
-	{
-		$json_str = "";
-		if (is_array($arr)) {
-			$pure_array = true;
-			$array_length = count($arr);
-			for ($i = 0; $i < $array_length; $i++) {
-				if (! isset($arr[$i])) {
-					$pure_array = false;
-					break;
-				}
-			}
-			if ($pure_array) {
-				$json_str ="[";
-				$temp = array();
-				for ($i = 0; $i < $array_length; $i++) {
-					$temp[] = sprintf("%s", php_json_encode($arr[$i]));
-				}
-				$json_str .= implode(",",$temp);
-				$json_str .= "]";
-			} else {
-				$json_str ="{";
-				$temp = array();
-				foreach ($arr as $key => $value) {
-					$temp[] = sprintf("\"%s\":%s", $key, json_encode($value));
-				}
-				$json_str .= implode(",",$temp);
-				$json_str .="}";
-			}
-		} else {
-			if(is_string($arr)) {
-				$json_str = "\"". json_encode_string($arr) . "\"";
-			} elseif (is_numeric($arr)) {
-				$json_str = $arr;
-			} else {
-				$json_str = "\"". json_encode_string($arr) . "\"";
-			}
-		}
-		return $json_str;
-	}
+	throw new Exception('PHP version at least 5.2 and support for JSON are needed');
 }
 
 /**
